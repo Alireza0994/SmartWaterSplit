1a581e5b665eec23980e71d0c79ec8a26adcee35
Alireza Salehi Zadeh <alirezasalehizadeh0994m@gmail.com>
2024-05-22 21:36:31 +0330

    update comment

--- SmartWaterSplit.ino
+++ SmartWaterSplit.ino
@@ -8,7 +8,7 @@
  this version is sipmpler coding style then first version
     so I tried to write less methods then last version
   Copyright 2024 by Alireza Salehi Zadeh
-  Thankyou
+  Thankyou | i am happy to use git
 */
 
 #include <LiquidCrystal_I2C.h> // for lcd
